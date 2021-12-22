6558e89fda4fa17372723d7cb91e81d30a39faf7
rgoshen <rick.goshen@gmail.com>
2021-12-21 19:48:07 -0700

    created user model

---
+++ models/User.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+// Declare the Schema of the Mongo model
+var UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  avatar: {
+    type: String,
+  },
+  date: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+//Export the model
+module.exports = mongoose.model("User", UserSchema);
