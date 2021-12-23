90d07d2c4b1c8743091d44dc2914c189e55a5868
rgoshen <rick.goshen@gmail.com>
2021-12-22 19:54:55 -0700

    added get profile by user id route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -112,4 +112,44 @@ router.post(
   }
 );
 
+/**
+ * @route   GET api/v1/profile
+ * @desc    Get all profiles
+ * @access  Public
+ */
+router.get("/", async (req, res) => {
+  try {
+    const profiles = await Profile.find().populate("user", ["name", "avatar"]);
+    res.json(profiles);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
+});
+
+/**
+ * @route   GET api/v1/profile/user/"user_id"
+ * @desc    Get profile by user_id
+ * @access  Public
+ */
+router.get("/user/:user_id", async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.params.user_id,
+    }).populate("user", ["name", "avatar"]);
+
+    if (!profile) {
+      return res.status(400).json({ errors: [{ msg: "Profile not found" }] });
+    }
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind === "ObjectId") {
+      return res.status(400).json({ errors: [{ msg: "Profile not found" }] });
+    }
+    res.status(500).send("Server error");
+  }
+});
+
 module.exports = router;
