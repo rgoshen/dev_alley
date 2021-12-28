0026eb0fbd96aed41b334986e9c65627ef180cfb
rgoshen <rick.goshen@gmail.com>
2021-12-28 10:29:06 -0700

    added authenticate to registration

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,11 @@
+import React, { useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
 import Login from "./components/auth/login";
 import Register from "./components/auth/register";
+import setAuthToken from "./utils/setAuthToken";
+import { loadUser } from "./actions/auth";
 import Alert from "./components/layout/Alert";
 
 // Redux
@@ -11,18 +14,28 @@ import store from "./store";
 
 import "./App.css";
 
-const App = () => (
-  <Provider store={store}>
-    <Router>
-      <Navbar />
-      <Alert />
-      <Routes>
-        <Route exact path='/' element={<Landing />} />
-        <Route exact path='register' element={<Register />} />
-        <Route exact path='login' element={<Login />} />
-      </Routes>
-    </Router>
-  </Provider>
-);
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
+
+const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+
+  return (
+    <Provider store={store}>
+      <Router>
+        <Navbar />
+        <Alert />
+        <Routes>
+          <Route exact path='/' element={<Landing />} />
+          <Route exact path='register' element={<Register />} />
+          <Route exact path='login' element={<Login />} />
+        </Routes>
+      </Router>
+    </Provider>
+  );
+};
 
 export default App;
