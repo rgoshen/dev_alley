17e2d5d39dab787fe52d73c51458a382c4abb1f8
rgoshen <rick.goshen@gmail.com>
2021-12-28 07:42:26 -0700

    fixed alert issue appearing under navbar

--- client/src/components/layout/Alert.js
+++ client/src/components/layout/Alert.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 
 const Alert = ({ alerts }) => (
-  <div className='alert-wrapper'>
+  <div className='container alert-wrapper'>
     {alerts.map((alert) => (
       <div key={alert.id} className={`alert alert-${alert.alertType}`}>
         {alert.msg}
