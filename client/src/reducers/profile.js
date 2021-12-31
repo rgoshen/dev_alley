f9e2f589e9252d00171b54ea3a154c4532c6017b
rgoshen <rick.goshen@gmail.com>
2021-12-31 15:33:53 -0700

    fixed security flaw with profile reducer

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -38,6 +38,7 @@ function profileReducer(state = initialState, action) {
         ...state,
         error: payload,
         loading: false,
+        profile: null,
       };
     case CLEAR_PROFILE:
       return {
