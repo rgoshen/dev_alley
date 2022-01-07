a7ce1a90799d498256900adccabb89151e29101e
rgoshen <rick.goshen@gmail.com>
2022-01-06 19:31:59 -0700

    ready for deployment

--- config/config.js
+++ config/config.js
@@ -2,18 +2,15 @@ require('dotenv').config();
 require('colors');
 
 const PORT = process.env.SERVER_PORT || 5001;
-const HOSTNAME = process.env.HOSTNAME;
 const SECRET_KEY = process.env.SECRET_KEY || 'secret-dev';
 
 console.log('========================================');
 console.log('DevAlley config: '.green);
 console.log('SECRET_KEY'.yellow, SECRET_KEY);
-console.log('HOSTNAME: '.yellow, HOSTNAME);
 console.log('PORT: '.yellow, PORT);
 console.log('========================================');
 
 module.exports = {
   PORT,
-  HOSTNAME,
   SECRET_KEY,
 };
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-const express = require("express");
-const { PORT, HOSTNAME } = require("./config/config");
-const connectDB = require("./config/db");
-const morgan = require("morgan");
+const express = require('express');
+const { PORT, HOSTNAME } = require('./config/config');
+const connectDB = require('./config/db');
+const morgan = require('morgan');
+const path = require('path');
 
 const app = express();
 
@@ -11,18 +12,28 @@ connectDB();
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 
 // Define Routes
-app.use("/api/v1/users", require("./routes/api/users"));
-app.use("/api/v1/auth", require("./routes/api/auth"));
-app.use("/api/v1/profile", require("./routes/api/profile"));
-app.use("/api/v1/posts", require("./routes/api/posts"));
+app.use('/api/v1/users', require('./routes/api/users'));
+app.use('/api/v1/auth', require('./routes/api/auth'));
+app.use('/api/v1/profile', require('./routes/api/profile'));
+app.use('/api/v1/posts', require('./routes/api/posts'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set the static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 app.listen(PORT, (err) => {
   if (err) {
-    console.error("Error starting server", err.stack);
+    console.error('Error starting server', err.stack);
   } else {
-    console.log(`Server running on http://${HOSTNAME}:${PORT}...`);
+    console.log(`Server running on Port: ${PORT}...`);
   }
 });
