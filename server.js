42f5453d9140780f8802654dc109b3503ad5e10e
rgoshen <rick.goshen@gmail.com>
2022-01-13 16:18:43 -0700

    remove config.js and move port to server.js

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
-const express = require("express");
-const { PORT } = require("./config/config");
-const connectDB = require("./config/db");
-const morgan = require("morgan");
-const path = require("path");
+const express = require('express');
+const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
@@ -12,27 +10,27 @@ connectDB();
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-app.use(morgan("dev"));
-
 // Define Routes
-app.use("/api/v1/users", require("./routes/api/users"));
-app.use("/api/v1/auth", require("./routes/api/auth"));
-app.use("/api/v1/profile", require("./routes/api/profile"));
-app.use("/api/v1/posts", require("./routes/api/posts"));
+app.use('/api/v1/users', require('./routes/api/users'));
+app.use('/api/v1/auth', require('./routes/api/auth'));
+app.use('/api/v1/profile', require('./routes/api/profile'));
+app.use('/api/v1/posts', require('./routes/api/posts'));
 
 // Serve static assets in production
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === 'production') {
   // Set the static folder
-  app.use(express.static("client/build"));
+  app.use(express.static('client/build'));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
+const PORT = 5001;
+
 app.listen(PORT, (err) => {
   if (err) {
-    console.error("Error starting server", err.stack);
+    console.error('Error starting server', err.stack);
   } else {
     console.log(`Server running on Port: ${PORT}...`);
   }
